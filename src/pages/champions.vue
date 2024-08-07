d2e842610620ae6c4b5d6e4543348c48f23515cf
lolpockets <auralielol@gmail.com>
2024-08-07 15:00:22 +0000

    Aug 7, 2024, 10:00 AM

--- src/pages/champions.vue
+++ src/pages/champions.vue
@@ -49,7 +49,7 @@ const champions = dataStore.champions;
           <input type="radio" name="champ-tabs" role="tab" class="tab w-64 font-normal text-2xl" aria-label="　 ♡ 　"
             checked="true" />
 
-          <div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6">
+          <div role="tabpanel" class="tab-content bg-secondary/10 border-base-300 rounded-box p-6">
             <div class="grid-container c-grid">
               <div class="grid-item champ" v-for="champion in champions">
 
