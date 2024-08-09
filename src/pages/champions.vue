30a98a1403348da2d8877c45233b371745074602
lolpockets <auralielol@gmail.com>
2024-08-09 16:01:52 +0000

    Aug 9, 2024, 11:01 AM

--- src/pages/champions.vue
+++ src/pages/champions.vue
@@ -78,8 +78,8 @@ const champions = dataStore.champions;
       </div>
 
       <div class="drawer-side">
-        <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
-        <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+        <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay z-10"></label>
+        <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4 z-20">
           <!-- Sidebar content here -->
           <li><a>Sidebar Item 1</a></li>
           <li><a>Sidebar Item 2</a></li>
