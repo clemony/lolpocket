6a498b90a9645033d172a0d451237adc4e90dc09
lolpockets <auralielol@gmail.com>
2024-08-09 23:00:52 +0000

    Aug 9, 2024, 6:00 PM

--- src/pages/champions.vue
+++ src/pages/champions.vue
@@ -91,7 +91,7 @@ function handleChampionClick(champion: Champion) {
 
       <div class="drawer-side z-20">
         <label for="my-drawer" aria-label="close sidebar" class="drawer-overlay"></label>
-        <div class="menu bg-base-200 text-base-content min-h-full w-80 px-4 pt-8 shadow-inner shadow-neutral/20">
+        <div class="menu  bg-base-200 text-base-content min-h-full w-80 px-4 pt-8 shadow-inner shadow-neutral/20">
           <!-- Sidebar content here -->
           <div v-if="selectedChampion" class="">
 
