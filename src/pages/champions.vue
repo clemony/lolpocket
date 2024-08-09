b0c26e459da816b0c19adaba2b0ecb42cce2ad2d
lolpockets <auralielol@gmail.com>
2024-08-09 21:07:07 +0000

    Aug 9, 2024, 4:07 PM

--- src/pages/champions.vue
+++ src/pages/champions.vue
@@ -126,6 +126,7 @@ function handleChampionClick(champion: Champion) {
             </div>
 
             <div role="tablist" class="tabs tabs-lifted mt-5">
+
               <input type="radio" name="ability-tabs" role="tab" class="tab"
                 :style="{ backgroundImage: `url(${selectedChampion.pImg})` }" aria-label="P" checked="true" />
               <div role="tabpanel" class="tab-content bg-base-100 border-base-300 rounded-box p-6 
