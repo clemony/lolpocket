0a537a4d48c9afac1d0814baf4504b8ebc5c5ef5
lolpockets <auralielol@gmail.com>
2024-08-10 01:09:19 +0000

    Aug 9, 2024, 8:09 PM

--- src/pages/champions.vue
+++ src/pages/champions.vue
@@ -111,7 +111,7 @@ function handleChampionClick(champion: Champion) {
                 </div>
                 <div class="col-start-2 col-span-2">
                   <h1 class="serif  text-[1.7rem] mb-0 pb-1">{{ selectedChampion.name }}</h1>
-                  <p class="text-xs tracking-wide align-middle uppercase pt-0 mt-0 pb-1">
+                  <p class="text-xs flex tracking-wide align-middle uppercase pt-0 mt-0 pb-1">
                     <Icon icon="ph:at-bold" class="inline" />
                     {{ selectedChampion.title }}
                   </p>
