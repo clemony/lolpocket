a04ffa248064f03dba0737c8b09d5e7952ce5ce4
lolpockets <auralielol@gmail.com>
2024-08-12 21:45:20 +0000

    Aug 12, 2024, 4:45 PM

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -264,13 +264,10 @@ onMounted(async () => {
 
 
     <div class="w-full h-screen m-0 p-0  pt-14 pr-4 col-start-2 overflow-scroll">
-
-
       <!-- Display the current component once loading is complete -->
       <!--<Transition name="slide-fade" mode="out-in">-->
       <component :is="currentComponent" />
       <!--</Transition>-->
-
     </div>
 
   </div>
