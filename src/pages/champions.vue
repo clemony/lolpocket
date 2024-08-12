59c6a095a138e74f00a5b8473b3724d1146706a2
lolpockets <auralielol@gmail.com>
2024-08-12 13:21:02 +0000

    Aug 12, 2024, 8:21 AM

--- src/pages/champions.vue
+++ src/pages/champions.vue
@@ -162,7 +162,7 @@ function handleChampionClick(champion: Champion) {
               <div
                 class="col-start-2 text-xs justify-content-end text-center justify-self-end relative right-3 flex gap-2 pb-1">
                 <code v-for="(tag, index) in selectedChampion.tags" :key="index"
-                  class="tracking-normal lowercase text-xs w-auto h-max flex flex-nowrap text-center mono"
+                  class="tracking-normal lowercase text-xs w-auto h-max flex flex-nowrap text-center font-mono"
                   :data-tag="tag">#{{ tag }}</code>
               </div>
             </div>
