417871ae4c7f1260616b6c61878d15a6e45aa388
lolpockets <auralielol@gmail.com>
2024-08-12 13:39:05 +0000

    Aug 12, 2024, 8:39 AM

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -150,7 +150,8 @@ onMounted(async () => {
             class="text-xs font-medium w-[470px] text-center h-[28px] shadow-inner bg-base-200/60 border-base-300 shadow-base-300 !outline-0 !outline-transparent rounded-[10px] !outline-none z-20 focus:ring-base-300" />
           <div tabindex="0"
             class="dropdown-content menu m-0 p-0 bg-base-100 w-[560px] rounded-md place-content-center z-[1] top-0 px-0 pt-0 shadow -ml-[45px]">
-            <Input tabindex="0" role="button" placeholder="" class="in-search z-20" />
+            <Input tabindex="0" role="button" placeholder=""
+              class="text-xs font-medium w-[470px] text-center h-[28px] shadow-inner bg-base-200/60 border-base-content/15 shadow-base-content/10 !outline-0 !outline-transparent !outline-none z-20 focus:ring-base-300" />
           </div>
         </div>
       </form>
