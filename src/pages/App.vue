f6a1777025ceb3e3f5ab0bc9601f06455c3bc1fa
lolpockets <auralielol@gmail.com>
2024-08-13 21:36:08 +0000

    Aug 13, 2024, 4:36 PM

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -60,7 +60,7 @@ function toggleMenu() {
     if (menu.value) {
 
       // Capture the initial state of the menu
-      // const state = Flip.getState(menu.value);
+      const state = Flip.getState(menu.value);
 
       var menu = menu.value;
       // Use gsap.utils.selector with a proper context
@@ -88,7 +88,7 @@ function toggleMenu() {
       ];
 
       // Use Flip.getState for all elements individually
-      const state = allElements.map(el => Flip.getState(el));
+      const states = allElements.map(el => Flip.getState(el));
 
 
       // Toggle the class on all elements
