94d2ae7825c5df280e315f46ac6d2cb947584eca
lolpockets <auralielol@gmail.com>
2024-08-13 22:07:49 +0000

    Aug 13, 2024, 5:07 PM

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -91,26 +91,26 @@ function toggleMenu() {
       ...nodetogglebutton,
       ...nodelabel
     ];
-
-    var tl = gsap.timeline();
-
-    tl.from(hideThese, {
-      opacity: 0, duration: 0.25, onComplete: function () {
-        this.targets().forEach(elem => elem.classList.add("hidden"))
-      }
-    });
-    tl.from(menu, { gridTemplateColumns: "80px auto", duration: 1 }, "<");
-    tl.to(nav, { borderRadius: "20px", paddingLeft: "3px", duration: 1 }, "<");
-    tl.to(nodeicon, {
-      margin: 0,
-      width: "1.35rem",
-      height: "1.35rem",
-      alignSelf: "center",
-      opacity: "0.8",
-      duration: 1
-    },
-      "<");
-    tl.to(node, { margin: "0.7rem 0", justifyContent: "center", justifyItems: "center", display: "flex", duration: 1 }, "<");
+    /*
+        var tl = gsap.timeline();
+    
+        tl.from(nodelabel, {
+          opacity: 0, duration: 0.25, onComplete: function () {
+            this.targets().forEach(elem => elem.classList.add("hidden"))
+          }
+        });
+        tl.from(menu, { gridTemplateColumns: "80px auto", duration: 1 }, "<");
+         tl.to(nav, { borderRadius: "20px", paddingLeft: "3px", duration: 1 }, "<");
+         tl.to(nodeicon, {
+           margin: 0,
+           width: "1.35rem",
+           height: "1.35rem",
+           alignSelf: "center",
+           opacity: "0.8",
+           duration: 1
+         },
+           "<");
+         tl.to(node, { margin: "0.7rem 0", justifyContent: "center", justifyItems: "center", display: "flex", duration: 1 }, "<");*/
 
   }
 
