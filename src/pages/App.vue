e1079aedd3e4f23e63b06904a3061514d98544a4
lolpockets <auralielol@gmail.com>
2024-08-13 23:15:02 +0000

    Aug 13, 2024, 6:15 PM

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -177,52 +177,52 @@ onMounted(async () => {
   </div>
 
 
+  <transition class="">
+    <div ref="menuRef" id="menu" :class="{ collapsed: false }"
+      class="w-screen grid grid-cols-[300px_auto] gap-4 m-0 p-0 ">
 
-  <div ref="menuRef" id="menu" :class="{ collapsed: false }"
-    class="w-screen grid grid-cols-[300px_auto] gap-4 m-0 p-0 ">
+      <div class="col-start-1">
 
-    <div class="col-start-1">
-
-      <div ref="nav" class="ml-5 mt-14 rounded-lg bg-base-100 border border-base-300
+        <div ref="nav" class="ml-5 mt-14 rounded-lg bg-base-100 border border-base-300
             shadow-base-300 shadow-lg nav">
 
-        <Tree v-model:selectionKeys="selectedKey" :value="nodes" selectionMode="single" :metaKeySelection="false"
-          @nodeSelect="onNodeSelect" id="tree1">
+          <Tree v-model:selectionKeys="selectedKey" :value="nodes" selectionMode="single" :metaKeySelection="false"
+            @nodeSelect="onNodeSelect" id="tree1">
 
 
-          <template #nodetogglebutton>
-          </template>
+            <template #nodetogglebutton>
+            </template>
 
-          <template #default="slotProps">
-            <transition class="label">
+            <template #default="slotProps">
+
               {{ slotProps.node.label }}
-            </transition>
-          </template>
+            </template>
 
-          <template #addon="slotProps">
-            <div ref="label">{{ slotProps.node.label }}</div>
+            <template #addon="slotProps">
+              <div ref="label">{{ slotProps.node.label }}</div>
 
-            <Button ref="label" variant="null" class="add-build cursor-pointer" title="create new build">
-              <Icon icon='ph:plus' class="add-fill" />
-            </Button>
-          </template>
+              <Button ref="label" variant="null" class="add-build cursor-pointer" title="create new build">
+                <Icon icon='ph:plus' class="add-fill" />
+              </Button>
+            </template>
 
-        </Tree>
+          </Tree>
+
+        </div>
+
+        <Toaster />
 
       </div>
 
-      <Toaster />
+      <div class="w-full h-screen m-0 p-0  pt-14 pr-4 col-start-2 overflow-scroll">
+        <!-- Display the current component once loading is complete -->
+        <!--<Transition name="slide-fade" mode="out-in">-->
+        <component :is="currentComponent" />
+        <!--</Transition>-->
+      </div>
 
     </div>
-
-    <div class="w-full h-screen m-0 p-0  pt-14 pr-4 col-start-2 overflow-scroll">
-      <!-- Display the current component once loading is complete -->
-      <!--<Transition name="slide-fade" mode="out-in">-->
-      <component :is="currentComponent" />
-      <!--</Transition>-->
-    </div>
-
-  </div>
+  </transition>
 </template>
 
 <style>
