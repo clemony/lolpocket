6c3214d97c54c057e218c3ec7ea58b6d9fb2f75a
lolpockets <auralielol@gmail.com>
2024-08-13 21:57:09 +0000

    Aug 13, 2024, 4:57 PM

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -93,14 +93,25 @@ function toggleMenu() {
         ...nodelabel
       ];
 
-      // Toggle the class on all elements
-      allElements.forEach(el => el.classList.add("minimize"));
-
       var tl = gsap.timeline();
-      tl.from(hideThese, { opacity: 0 }, "<");
-      tl.from(menu, {});
 
-
+      tl.from(hideThese, {
+        opacity: 0, duration: 0.25, onComplete: function () {
+          this.targets().forEach(elem => elem.classList.add("hidden"))
+        }
+      });
+      tl.from(menu, { gridTemplateColumns: "80px auto", duration: 1 }, "<");
+      tl.to(".nav", { borderRadius: "20px", paddingLeft: "3px", duration: 1 }, "<");
+      tl.to(".nodeicon", {
+        margin: 0,
+        width: "1.35rem",
+        height: "1.35rem",
+        alignSelf: "center",
+        opacity: "0.8",
+        duration: 1
+      },
+        "<");
+      tl.to(".node", { margin: "0.7rem 0", justifyContent: "center", justifyItems: "center", display: "flex", duration: 1 }, "<");
     }
   })
 };
