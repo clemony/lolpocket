3eecaab28d5ed59284c0ae79718a618068cb8e42
lolpockets <auralielol@gmail.com>
2024-08-14 12:35:18 +0000

    Aug 14, 2024, 7:35 AM

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -92,7 +92,7 @@ function toggleMenu() {
       this.targets().forEach(elem => elem.classList.add("hidden"))
     }
   });
-  tl.from(menu, { gridTemplateColumns: "80px auto", duration: 2 }, "<");
+  tl.from(menu, { gridTemplateColumns: "80px auto", duration: 4 }, "");
   tl.to(nav, { borderRadius: "20px", duration: 1 }, "<");
   tl.to(rootchildren, {
     display: "flex",
