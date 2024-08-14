1aae9bb321fdff9d94a11e3a6f67096c22eeb019
lolpockets <auralielol@gmail.com>
2024-08-14 11:17:11 +0000

    Aug 14, 2024, 6:17 AM

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -59,6 +59,44 @@ const menuRef = ref<HTMLDivElement | null>(null);
 
 function toggleMenu() {
 
+  var tl = gsap.timeline();
+  console.log(menuRef);
+  var menu = menuRef.value;
+  console.log(menu);
+  const m = gsap.utils.selector(menuRef.value);
+  const nav = m(".nav");
+  const node = m(".node");
+  const nodecontent = m(".nodecontent");
+  const nodelabel = m(".nodelabel");
+  const nodeicon = m(".nodeicon");
+  const nodechildren = m(".nodechildren");
+  const nodetogglebutton = m(".nodetogglebutton");
+  const rootchildren = m(".rootchildren");
+  const hideThese = [
+    ...nodechildren,
+    ...nodetogglebutton,
+    ...nodelabel
+  ];
+
+  tl.from(hideThese, {
+    opacity: 0, duration: 0.25, onComplete: function () {
+      this.targets().forEach(elem => elem.classList.add("hidden"))
+    }
+  });
+  tl.from(menu, { gridTemplateColumns: "80px auto", duration: 1 }, "<");
+  tl.to(nav, { borderRadius: "20px", paddingLeft: "3px", duration: 1 }, "<");
+  tl.to(nodeicon, {
+    margin: 0,
+    width: "1.35rem",
+    height: "1.35rem",
+    alignSelf: "center",
+    opacity: "0.8",
+    duration: 1
+  },
+    "<");
+  tl.to(node, { margin: "0.7rem 0", justifyContent: "center", justifyItems: "center", display: "flex", duration: 1 }, "<");
+  return tl;
+
 };
 
 const minimized = ref(false);
@@ -84,44 +122,6 @@ onMounted(async () => {
   NodeService.getTreeNodes().then((data: null) => (nodes.value = data));
   nextTick(() => {
     console.log(menuRef);
-    var tl = gsap.timeline();
-
-    console.log(menuRef);
-    var menu = menuRef.value;
-    console.log(menu);
-    const m = gsap.utils.selector(menuRef.value);
-    const nav = m(".nav");
-    const node = m(".node");
-    const nodecontent = m(".nodecontent");
-    const nodelabel = m(".nodelabel");
-    const nodeicon = m(".nodeicon");
-    const nodechildren = m(".nodechildren");
-    const nodetogglebutton = m(".nodetogglebutton");
-    const rootchildren = m(".rootchildren");
-    const hideThese = [
-      ...nodechildren,
-      ...nodetogglebutton,
-      ...nodelabel
-    ];
-
-    tl.from(hideThese, {
-      opacity: 0, duration: 0.25, onComplete: function () {
-        this.targets().forEach(elem => elem.classList.add("hidden"))
-      }
-    });
-    tl.from(menu, { gridTemplateColumns: "80px auto", duration: 1 }, "<");
-    tl.to(nav, { borderRadius: "20px", paddingLeft: "3px", duration: 1 }, "<");
-    tl.to(nodeicon, {
-      margin: 0,
-      width: "1.35rem",
-      height: "1.35rem",
-      alignSelf: "center",
-      opacity: "0.8",
-      duration: 1
-    },
-      "<");
-    tl.to(node, { margin: "0.7rem 0", justifyContent: "center", justifyItems: "center", display: "flex", duration: 1 }, "<");
-    return tl;
   })
 
 });
