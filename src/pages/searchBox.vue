6504dda73ccf29f2c4db286f067dd13666a7220e
lolpockets <auralielol@gmail.com>
2024-08-15 15:43:38 +0000

    Aug 15, 2024, 10:43 AM

--- src/pages/searchBox.vue
+++ src/pages/searchBox.vue
@@ -12,7 +12,7 @@ import { Icon } from "@iconify/vue";
             <Input tabindex="0" role="button" placeholder=""
                 class="text-xs font-medium w-[470px] text-center h-[28px] shadow-inner bg-base-200/60 border-base-300 shadow-base-300 !outline-0 !outline-transparent rounded-[10px] !outline-none z-20 focus:ring-base-300" />
             <div tabindex="0"
-                class="dropdown-content menu m-0 p-0 bg-base-100 w-[500px] rounded-[10px] place-content-center z-[1] top-[-1px] px-0 pt-[2px] shadow-lg -ml-[15px]">
+                class="dropdown-content menu m-0 p-0 bg-base-100 w-[500px] rounded-[10px] place-content-center z-[1] top-[-1px] px-0 pt-[2px] shadow-lg -ml-[15px] backdrop-blur-md bg-opacity-80">
                 <Input tabindex="0" role="button" placeholder=""
                     class="text-xs font-medium max-w-[470px] text-center h-[28px] shadow-inner bg-base-200/60 border-base-content/15 shadow-base-content/10 rounded-[10px] !outline-0 !outline-transparent !outline-none z-20 focus:ring-base-300" />
 
@@ -73,9 +73,16 @@ import { Icon } from "@iconify/vue";
     @apply mt-3;
 }
 
-.tab {}
+.tab,
+.tab-active {
+    @apply bg-transparent;
+}
 
 .radio {
     @apply radio-xs ml-2;
+}
+
+.tab-panel {
+    @apply bg-transparent;
 }
 </style>
