f3c9ae406495a549c3858e61a73ab7ce526c3f59
lolpockets <auralielol@gmail.com>
2024-08-15 12:42:50 +0000

    Aug 15, 2024, 7:42 AM

--- src/data/albums.ts
+++ src/data/albums.ts
@@ -6,25 +6,25 @@ export interface Album {
   
   export const listenNowAlbums: Album[] = [
     {
-      name: 'React Rendezvous',
-      artist: 'Ethan Byte',
+      name: 'Patch 14.15',
+      artist: 'Riot Games',
       cover:
         'https://images.unsplash.com/photo-1611348586804-61bf6c080437?w=300&dpr=2&q=80',
     },
     {
-      name: 'Async Awakenings',
+      name: 'PBE Update',
       artist: 'Nina Netcode',
       cover:
         'https://images.unsplash.com/photo-1468817814611-b7edf94b5d60?w=300&dpr=2&q=80',
     },
     {
-      name: 'The Art of Reusability',
+      name: 'Thing',
       artist: 'Lena Logic',
       cover:
         'https://images.unsplash.com/photo-1528143358888-6d3c7f67bd5d?w=300&dpr=2&q=80',
     },
     {
-      name: 'Stateful Symphony',
+      name: 'Thing',
       artist: 'Beth Binary',
       cover:
         'https://images.unsplash.com/photo-1490300472339-79e4adc6be4a?w=300&dpr=2&q=80',
@@ -33,39 +33,21 @@ export interface Album {
   
   export const madeForYouAlbums: Album[] = [
     {
-      name: 'Thinking Components',
-      artist: 'Lena Logic',
+      name: 'Patch 14.15',
+      artist: 'Riot Games',
       cover:
-        'https://images.unsplash.com/photo-1615247001958-f4bc92fa6a4a?w=300&dpr=2&q=80',
+        'https://images.unsplash.com/photo-1611348586804-61bf6c080437?w=300&dpr=2&q=80',
     },
     {
-      name: 'Functional Fury',
-      artist: 'Beth Binary',
-      cover:
-        'https://images.unsplash.com/photo-1513745405825-efaf9a49315f?w=300&dpr=2&q=80',
-    },
-    {
-      name: 'React Rendezvous',
-      artist: 'Ethan Byte',
-      cover:
-        'https://images.unsplash.com/photo-1614113489855-66422ad300a4?w=300&dpr=2&q=80',
-    },
-    {
-      name: 'Stateful Symphony',
-      artist: 'Beth Binary',
-      cover:
-        'https://images.unsplash.com/photo-1446185250204-f94591f7d702?w=300&dpr=2&q=80',
-    },
-    {
-      name: 'Async Awakenings',
+      name: 'PBE Update',
       artist: 'Nina Netcode',
       cover:
         'https://images.unsplash.com/photo-1468817814611-b7edf94b5d60?w=300&dpr=2&q=80',
     },
     {
-      name: 'The Art of Reusability',
+      name: 'Thing',
       artist: 'Lena Logic',
       cover:
-        'https://images.unsplash.com/photo-1490300472339-79e4adc6be4a?w=300&dpr=2&q=80',
+        'https://images.unsplash.com/photo-1528143358888-6d3c7f67bd5d?w=300&dpr=2&q=80',
     },
   ]
