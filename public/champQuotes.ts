3587eee2398f8b833a3fb65d952644e09ce882cf
lolpockets <auralielol@gmail.com>
2024-08-15 20:28:27 +0000

    Aug 15, 2024, 3:28 PM

--- public/champQuotes.ts
+++ public/champQuotes.ts
@@ -4,5 +4,8 @@ const quotes = [
     "A Crownguard and a mage? Wouldn't want to be her.",
     "Sylas? He hasn't changed. And my answer is still no.",
     "It's good to see Taric hasn't changed. And bad. It's both.",
-
+    "Stay positive.",
+    "Kat and Garen sitting in a tree~ And you better just be holding hands or I'm telling!",
+    "I'm sure your wife forgives you for getting her soul taken by a remorseless creature of darkness!",
+    
 ]
