f8ce14447e43118618cb34573f3c55c319f3d09d
lolpockets <auralielol@gmail.com>
2024-08-15 12:33:14 +0000

    Aug 15, 2024, 7:33 AM

--- src/pages/modules/searchBox.vue
+++ src/pages/modules/searchBox.vue
@@ -4,6 +4,7 @@ import { Icon } from "@iconify/vue";
 
 <template>
     <form class="text-center justify-items-center rounded-lg z-0">
+        <Icon icon="ph:magnifying-glass" class="absolute right-3 top-[5.5px] size-4 z-20 text-base-content/50" />
         <div class="relative ">
             <Icon icon="ph:magnifying-glass" class="absolute right-3 top-[5.5px] size-4 z-20 text-base-content/50" />
             <Input tabindex="0" role="button" placeholder=""
@@ -12,6 +13,8 @@ import { Icon } from "@iconify/vue";
                 class="dropdown-content menu m-0 p-0 bg-base-100 w-[560px] rounded-md place-content-center z-[1] top-0 px-0 pt-0 shadow -ml-[45px]">
                 <Input tabindex="0" role="button" placeholder=""
                     class="text-xs font-medium w-[470px] text-center h-[28px] shadow-inner bg-base-200/60 border-base-content/15 shadow-base-content/10 rounded-[10px] !outline-0 !outline-transparent !outline-none z-20 focus:ring-base-300" />
+                <Icon icon="ph:magnifying-glass"
+                    class="absolute right-3 top-[5.5px] size-4 z-20 text-base-content/50" />
             </div>
         </div>
     </form>
