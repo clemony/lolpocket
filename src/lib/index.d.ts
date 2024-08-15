6efe63dd8246c37863d06fd8ce867cb9d2dcf415
lolpockets <auralielol@gmail.com>
2024-08-15 12:00:43 +0000

    Aug 15, 2024, 7:00 AM

--- src/lib/index.d.ts
+++ src/lib/index.d.ts
@@ -1,5 +1,5 @@
 declare module 'vue3-resize-text';
-declare module '@grapoza/vue-tree';
+declare module '@iconify/vue';
 declare module '@/pages/tree.vue';
 declare module '@/service/NodeService';
 declare module '@/pages/App.vue';
