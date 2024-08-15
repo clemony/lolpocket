1d6db7716c0fd46bf025784d7b3ddcd0280ed6e9
lolpockets <auralielol@gmail.com>
2024-08-15 14:00:05 +0000

    Aug 15, 2024, 9:00 AM

--- src/pages/searchBox.vue
+++ src/pages/searchBox.vue
@@ -10,7 +10,7 @@ import { Icon } from "@iconify/vue";
             <Input tabindex="0" role="button" placeholder=""
                 class="text-xs font-medium w-[470px] text-center h-[28px] shadow-inner bg-base-200/60 border-base-300 shadow-base-300 !outline-0 !outline-transparent rounded-[10px] !outline-none z-20 focus:ring-base-300" />
             <div tabindex="0"
-                class="dropdown-content menu m-0 p-0 bg-base-100 w-[560px] rounded-md place-content-center z-[1] top-0 px-0 pt-0 shadow -ml-[45px]">
+                class="dropdown-content menu m-0 p-0 bg-base-100 w-[530px] rounded-md place-content-center z-[1] top-0 px-0 pt-0 shadow -ml-[45px]">
                 <Input tabindex="0" role="button" placeholder=""
                     class="text-xs font-medium w-[470px] text-center h-[28px] shadow-inner bg-base-200/60 border-base-content/15 shadow-base-content/10 rounded-[10px] !outline-0 !outline-transparent !outline-none z-20 focus:ring-base-300" />
 
@@ -50,9 +50,7 @@ import { Icon } from "@iconify/vue";
     @apply mt-3;
 }
 
-.tab {
-    @apply w-24;
-}
+.tab {}
 
 .checkbox {
     @apply checkbox-sm ml-2;
