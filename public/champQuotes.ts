e52a2ecac741ad9b320204f3d14805d8fc0806b1
lolpockets <auralielol@gmail.com>
2024-08-15 20:26:08 +0000

    Aug 15, 2024, 3:26 PM

--- public/champQuotes.ts
+++ public/champQuotes.ts
@@ -1,5 +1,8 @@
-const quotes = {
-        "Braum! What are you doing over there?! Are you drunk?",
-         "So, Braum. No shirt, huh? And I don't have pants. Yay, Freljord."
+const quotes = [
+    "Braum! What are you doing over there?! Are you drunk?",
+    "So, Braum. No shirt, huh? And I don't have pants. Yay, Freljord.",
+    "A Crownguard and a mage? Wouldn't want to be her.",
+    "Sylas? He hasn't changed. And my answer is still no.",
+    "It's good to see Taric hasn't changed. And bad. It's both.",
 
-}
+]
