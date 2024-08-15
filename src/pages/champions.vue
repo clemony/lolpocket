d70684288f16e85a1e2ccfcecfb1da08be0de180
lolpockets <auralielol@gmail.com>
2024-08-15 21:41:55 +0000

    Aug 15, 2024, 4:41 PM

--- src/pages/champions.vue
+++ src/pages/champions.vue
@@ -72,7 +72,7 @@ function handleChampionClick(champion: Champion) {
 
         <div id="" class="main flex-1">
           <div role="tablist" class="tabs layout">
-            <div v-for="value in randomQuote">{{ value }}</div>
+            <div v-for="any in randomQuote">{{ randomQuote }}</div>
             <input type="radio" name="champ-tabs" role="tab" class="tab after:w-36 font-semibold" aria-label="Champions"
               checked="true" />
 
