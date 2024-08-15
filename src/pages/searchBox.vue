978492c424cf9f089f779273ff2ff958e37c5e10
lolpockets <auralielol@gmail.com>
2024-08-15 16:29:23 +0000

    Aug 15, 2024, 11:29 AM

--- src/pages/searchBox.vue
+++ src/pages/searchBox.vue
@@ -14,7 +14,7 @@ import { Icon } from "@iconify/vue";
             <Input tabindex="0" role="button" placeholder=""
                 class="text-xs font-medium w-[470px] text-center h-[28px] shadow-inner bg-base-200/60 border-base-300 shadow-base-300 !outline-0 !outline-transparent rounded-[10px] !outline-none z-20 focus:ring-base-300" />
             <div tabindex="0"
-                class="dropdown-content menu m-0 p-0 bg-base-100 w-[500px] rounded-[10px] place-content-center z-[1] top-[-1px] px-0 pt-[1px] shadow-lg -ml-[15px] backdrop-blur-md bg-opacity-80">
+                class="dropdown-content menu m-0 p-0 bg-base-100 w-[500px] rounded-bl-[10px] rounded-br-[10px] place-content-center z-[1] top-[-4px] px-0 pt-[3px] shadow-lg -ml-[15px] border border-base-300 backdrop-blur-md bg-opacity-80">
                 <Input tabindex="0" role="button" placeholder=""
                     class="text-xs font-medium max-w-[470px] text-center h-[28px] shadow-inner bg-base-200/60 border-base-content/15 shadow-base-content/10 rounded-[10px] !outline-0 !outline-transparent !outline-none z-20 focus:ring-base-300" />
 
