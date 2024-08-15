ff0011a404b58f74db498194ba2f31228ce5c8b8
lolpockets <auralielol@gmail.com>
2024-08-15 14:04:34 +0000

    Aug 15, 2024, 9:04 AM

--- src/pages/searchBox.vue
+++ src/pages/searchBox.vue
@@ -15,7 +15,7 @@ import { Icon } from "@iconify/vue";
                     class="text-xs font-medium max-w-[470px] text-center h-[28px] shadow-inner bg-base-200/60 border-base-content/15 shadow-base-content/10 rounded-[10px] !outline-0 !outline-transparent !outline-none z-20 focus:ring-base-300" />
 
                 <div role="tablist" class="tabs tabs-lifted">
-                    <a role="tab" class="tab">
+                    <a role="tab" class="tab *:text-xs">
                         <div class="form-control">
                             <label class="label cursor-pointer">
                                 <span class="label-text">Remember me</span>
