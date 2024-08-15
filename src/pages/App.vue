fa60c9c01936fecca692cbbb199c4aa86e2f3424
lolpockets <auralielol@gmail.com>
2024-08-15 11:47:36 +0000

    Aug 15, 2024, 6:47 AM

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -277,11 +277,7 @@ onMounted(async () => {
           <Icon icon="ph:magnifying-glass" class="absolute right-3 top-[5.5px] size-4 z-20 text-base-content/50" />
           <Input tabindex="0" role="button" placeholder=""
             class="text-xs font-medium w-[470px] text-center h-[28px] shadow-inner bg-base-200/60 border-base-300 shadow-base-300 !outline-0 !outline-transparent rounded-[10px] !outline-none z-20 focus:ring-base-300" />
-          <div tabindex="0"
-            class="dropdown-content menu m-0 p-0 bg-base-100 w-[560px] rounded-md place-content-center z-[1] top-0 px-0 pt-0 shadow -ml-[45px]">
-            <Input tabindex="0" role="button" placeholder=""
-              class="text-xs font-medium w-[470px] text-center h-[28px] shadow-inner bg-base-200/60 border-base-content/15 shadow-base-content/10 !outline-0 !outline-transparent !outline-none z-20 focus:ring-base-300" />
-          </div>
+
         </div>
       </form>
     </div>
