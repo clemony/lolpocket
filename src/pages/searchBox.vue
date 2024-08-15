4c786025ebae063878661e313f8c841ba51537f5
lolpockets <auralielol@gmail.com>
2024-08-15 15:49:28 +0000

    Aug 15, 2024, 10:49 AM

--- src/pages/searchBox.vue
+++ src/pages/searchBox.vue
@@ -17,7 +17,7 @@ import { Icon } from "@iconify/vue";
                     class="text-xs font-medium max-w-[470px] text-center h-[28px] shadow-inner bg-base-200/60 border-base-content/15 shadow-base-content/10 rounded-[10px] !outline-0 !outline-transparent !outline-none z-20 focus:ring-base-300" />
 
 
-                <div role="tablist" class="tabs tabs-lifted">
+                <div role="tablist" class="tabs tabs-lifted grid grid-cols-1">
                     <a role="tab" class="tab tab-active">
                         <div class="form-control">
                             <label class="label cursor-pointer">
