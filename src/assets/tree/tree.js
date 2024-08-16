57e962510215c3856e0153d7d1e308e71b90977f
lolpockets <auralielol@gmail.com>
2024-08-16 21:13:27 +0000

    Aug 16, 2024, 4:13 PM

--- src/assets/tree/tree.js
+++ src/assets/tree/tree.js
@@ -37,11 +37,11 @@ export default {
       // Spacing
 
       // Colors
-      context.selected ? "bg-base-200 shadow-sm text-base-content" : "bg-base-100 text-base-content hover:bg-base-300 hover:text-base-content",
+      context.selected ? "bg-base-200 shadow-sm text-base-content" : "bg-base-100 text-base-content hover:bg-base-300 hover:text-base-content hover:shadow-sm",
 
       // States
       {
-        "hover:bg-base-300 hover:text-base-content": (props.selectionMode == "single" || props.selectionMode == "multiple") && !context.selected,
+        "hover:bg-base-300 hover:text-base-content hover:shadow-sm": (props.selectionMode == "single" || props.selectionMode == "multiple") && !context.selected,
       },
 
       // Transition
