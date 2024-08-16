313fccf06dba427443dec7bb6cce8f6321c5db53
lolpockets <auralielol@gmail.com>
2024-08-16 23:54:22 +0000

    Aug 16, 2024, 6:54 PM

--- src/pages/searchBox.vue
+++ src/pages/searchBox.vue
@@ -9,13 +9,13 @@ const query = ref('');
 
         <div class="relative max-w-[470px]">
             <div
-                class=" w-full  shadow-inner bg-base-200/60 border-base-300 text-base-content/60 italic shadow-base-300  rounded-[10px]  z-20 focus:*:text-base-content focus:*:ring-base-300 focus:*:not-italic">
+                class=" w-full  shadow-inner bg-base-200/60 border-base-300 text-base-content/60 shadow-base-300  rounded-[10px]  z-20 focus:*:text-base-content focus:*:ring-base-300 focus:*:not-italic">
                 <div class="absolute right-3 top-[5.5px] flex gap-2.5 *:size-4 z-20 text-base-content/50">
                     <Icon icon="teenyicons:filter-outline" />
                     <Icon icon="ph:magnifying-glass" />
                 </div>
                 <Input tabindex="0" role="" v-model="query" placeholder=""
-                    class="italic h-[28px] text-xs !outline-0 !outline-transparent !outline-none rounded-[10px] "
+                    class="italic h-[28px] text-xs !outline-0 !outline-transparent !outline-none rounded-[10px] m"
                     @keydown.enter.prevent />
             </div>
             <!--<div tabindex=" 60"
