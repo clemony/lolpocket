eb244c5c3c38122165ae082bb71fe3ac1f8a93c5
lolpockets <auralielol@gmail.com>
2024-08-17 14:14:35 +0000

    Aug 17, 2024, 9:14 AM

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -15,10 +15,10 @@ const hasSource2 = ref('false');
 
 <template>
 
-    <div class="flex absolute overflow-x-scroll h-64 content-center gap-5 news-after scrollbar-hide left-0 z-0">
+    <div class="flex absolute overflow-x-scroll h-64 content-center gap-5 top-32 scrollbar-hide left-0 z-0">
 
         <div v-for="article in NewsArticles"
-            class="card !bg-base-100/60  backdrop-blur-m  h-56 text-sm shadow-lg first:w-64px first:min-w-96 min-w-56 z-0 *:z-0">
+            class="card !bg-base-100/60 h-56 text-sm shadow-lg first:w-64px first:min-w-96 min-w-56 z-0 *:z-0">
             <figure>
                 <img :src="article.image" :alt="article.title + ' image'" />
             </figure>
