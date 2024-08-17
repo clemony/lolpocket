2412b84c9fea82d41abeedce11fb4ac5d10bd782
lolpockets <auralielol@gmail.com>
2024-08-17 17:19:17 +0000

    Aug 17, 2024, 12:19 PM

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -16,7 +16,7 @@ const hasSource2 = ref('false');
 
 
 
-    <div class="w-full py-4 grid gap-y-3  m-0 ">
+    <div class="w-full py-4 grid gap-y-3  m-0 pointer-events-none z-0">
 
 
         <div class="gap-2 flex items-end">
