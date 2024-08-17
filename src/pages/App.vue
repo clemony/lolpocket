6ac551ffa4cf56b2f81e9ce1be7d0a488fefcfaf
lolpockets <auralielol@gmail.com>
2024-08-17 20:19:38 +0000

    Aug 17, 2024, 3:19 PM

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -160,7 +160,10 @@ onMounted(async () => {
   <div ref="menuRef" id="menu"
     class="w-screen grid grid-cols-[280px_calc(100%-280px)] gap-6  m-0 p-0 transition-all duration-700 delay-200 z-0">
 
-    <div class="col-start-1 z-10 p-4 ">
+
+    <div class="col-start-1 z-10">
+
+
 
       <News v-if="isHome" class="pl-[286px] !overflow-x-scroll" />
 
