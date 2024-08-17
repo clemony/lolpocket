0aac6451422e4e9ab2b78e3ca58cddef9937fbd3
lolpockets <auralielol@gmail.com>
2024-08-17 07:21:54 +0000

    Aug 17, 2024, 2:21 AM

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -28,7 +28,7 @@ const hasSource2 = ref('false');
             </p>
         </div>
 
-        <div class="grid grid-cols-4 overflow-x-scroll h-64 content-center gap-5">
+        <div class="grid grid-cols-[2fr_repeat(1fr)] overflow-x-scroll h-64 content-center gap-5">
 
             <div v-for="article in NewsArticles" class="card bg-base-100 w-54 h-56 text-sm shadow-lg">
                 <figure>
