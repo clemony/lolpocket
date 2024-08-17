45d8f8e21cec8f9045adf4aec2e6277e4178e55f
lolpockets <auralielol@gmail.com>
2024-08-17 17:31:11 +0000

    Aug 17, 2024, 12:31 PM

--- src/pages/modules/news.vue
+++ src/pages/modules/news.vue
@@ -56,7 +56,8 @@ const NewsArticles: article[] = [
 
 </script>
 <template>
-  <div class="flex w-full pr-6 absolute overflow-x-scroll h-64 content-center gap-5 top-32 scrollbar-hide left-0 !z-1">
+  <div
+    class="flex w-full pr-6 ml-5 absolute overflow-x-scroll h-64 content-center gap-5 top-32 scrollbar-hide left-0 !z-1">
 
     <div v-for="article in NewsArticles"
       class="card !bg-base-100/60 h-56 text-sm shadow-lg first:w-64px first:min-w-96 min-w-56 !z-0 !*:z-0">
