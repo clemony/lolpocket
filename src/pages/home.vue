be21e198d5f345dca0614556e7ff0af6b0f78b51
lolpockets <auralielol@gmail.com>
2024-08-17 04:30:58 +0000

    Aug 16, 2024, 11:30 PM

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -10,48 +10,40 @@ interface article {
 
 <template>
 
-    <div class="hidden w-screen h-screen md:block">
+    <div class="w-full">
 
-        <div class="">
-            <div class="bg-background">
-                <div class="grid lg:grid-cols-5">
-                    <div class="col-span-3 lg:col-span-4">
-                        <div class="h-full px-4 py-6 lg:px-8">
 
-                            <div class="mt-6 space-y-1">
-                                <h2 class="text-2xl font-semibold tracking-tight">
-                                    League News
-                                </h2>
-                                <p class="text-sm text-muted-foreground">
-                                    Stay on top of the happening things.
-                                </p>
-                            </div>
+        <div class="mt-6 space-y-1">
+            <h2 class="text-2xl font-semibold tracking-tight">
+                League News
+            </h2>
+            <p class="text-sm text-muted-foreground">
+                Stay on top of the happening things.
+            </p>
+        </div>
 
-                            <div class="flex">
+        <div class="flex overflow-x-scroll h-64 gap-5">
 
-                                <div v-for="article in NewsArticles" class="card bg-base-100 w-60 text-sm shadow-xl">
-                                    <figure>
-                                        <img :src="article.image" :alt="article.title + ' image'" />
-                                    </figure>
-                                    <div class="card-body">
-                                        <h2 class="card-title">
-                                            Shoes!
-                                            <div class="badge badge-secondary">NEW</div>
-                                        </h2>
-                                        <p>If a dog chews shoes whose shoes does he choose?</p>
-                                        <div class="card-actions justify-end">
-                                            <div class="badge badge-outline">Fashion</div>
-                                            <div class="badge badge-outline">Products</div>
-                                        </div>
-                                    </div>
-                                </div>
-
-                            </div>
-
-                        </div>
+            <div v-for="article in NewsArticles" class="card bg-base-100 w-60 text-sm shadow-xl">
+                <figure>
+                    <img :src="article.image" :alt="article.title + ' image'" />
+                </figure>
+                <div class="card-body">
+                    <h2 class="card-title">
+                        Shoes!
+                        <div class="badge badge-secondary">NEW</div>
+                    </h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div class="card-actions justify-end">
+                        <div class="badge badge-outline">Fashion</div>
+                        <div class="badge badge-outline">Products</div>
                     </div>
                 </div>
             </div>
+
         </div>
+
     </div>
+
+
 </template>
