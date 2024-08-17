8530eb0bcf6746caad3e2e8fb9e5e42a41d660cb
lolpockets <auralielol@gmail.com>
2024-08-17 13:24:53 +0000

    Aug 17, 2024, 8:24 AM

--- src/pages/settings.vue
+++ src/pages/settings.vue
@@ -124,34 +124,32 @@ function updateTheme(theme: string) {
 
 
         <div class="card bg-base-200 rounded-box grid h-20 place-items-center">
-          <div class="text-sm grid align-middle items-center">
-            <p class="">Thanks for trying the app! </p>
-            <p class="inline-flex gap-1 items-baseline"> Much
-              <Icon icon="typcn:heart-outline" class="size-3" />from Clem's human.
-            </p>
-            <p class="inline-flex gap-1 items-center mt-2">
-              <Icon icon="ph:github-logo" />
-              <Icon icon="ph:at" /><span>clemony</span>
-            </p>
-          </div>
-        </div>
-        <div class="divider text-xs">MADE WITH</div>
-        <div class="card bg-base-200 rounded-box grid h-20 place-items-center">
-          <div class="text-base-content/85 flex space-x-2 *:size-6">
-            <Icon icon="fontisto:vuejs" />
-
-            <Icon icon="tabler:brand-vite" />
-            <Icon icon="devicon-plain:tauri" />
-            <Icon icon="file-icons:typescript" />
-            <Icon icon="codicon:vscode-insiders" />
-            <Icon icon="simple-icons:nodedotjs" />
-            <Icon icon="simple-icons:tailwindcss" />
-            <img class="grayscale contrast-200 brightness-50" src="/img/ui/daisyui.svg" />
-          </div>
+          <p class="">Thanks for trying the app! </p>
+          <p class="inline-flex gap-1 items-baseline"> Much
+            <Icon icon="typcn:heart-outline" class="size-3" />from Clem's human.
+          </p>
+          <p class="inline-flex gap-1 items-center mt-2">
+            <Icon icon="ph:github-logo" />
+            <Icon icon="ph:at" /><span>clemony</span>
+          </p>
         </div>
       </div>
+      <div class="divider text-xs">MADE WITH</div>
+      <div class="card bg-base-200 rounded-box grid h-20 place-items-center">
+        <div class="text-base-content/85 flex space-x-2 *:size-6">
+          <Icon icon="fontisto:vuejs" />
 
+          <Icon icon="tabler:brand-vite" />
+          <Icon icon="devicon-plain:tauri" />
+          <Icon icon="file-icons:typescript" />
+          <Icon icon="codicon:vscode-insiders" />
+          <Icon icon="simple-icons:nodedotjs" />
+          <Icon icon="simple-icons:tailwindcss" />
+          <img class="grayscale contrast-200 brightness-50" src="/img/ui/daisyui.svg" />
+        </div>
+      </div>
     </div>
+
   </div>
 
 </template>
