d364dcf988e00cab09d950d2e821864d65763170
lolpockets <auralielol@gmail.com>
2024-08-17 07:30:35 +0000

    Aug 17, 2024, 2:30 AM

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -31,7 +31,7 @@ const hasSource2 = ref('false');
         <div class="flex overflow-x-scroll h-64 content-center gap-5">
 
             <div v-for="article in NewsArticles"
-                class="card bg-base-100  h-56 text-sm shadow-lg first:w-64px first:min-w-86 min-w-56">
+                class="card bg-base-100  h-56 text-sm shadow-lg first:w-64px first:min-w-96 min-w-56">
                 <figure>
                     <img :src="article.image" :alt="article.title + ' image'" />
                 </figure>
