ba43b8c8e9a8f39d14fa893131a530f0c1ec6256
lolpockets <auralielol@gmail.com>
2024-08-17 15:45:51 +0000

    Aug 17, 2024, 10:45 AM

--- src/pages/searchBox.vue
+++ src/pages/searchBox.vue
@@ -9,7 +9,7 @@ const query = ref('');
 
         <div class="relative max-w-[470px]">
             <div
-                class=" w-full  shadow-inner bg-base-200/60 border-base-content/30 ring-base-content/10 ring-offset-base-content/20 focus:ring-base-content/25 text-base-content/60  rounded-[10px]  z-20 focus:*:text-base-content focus:*:not-italic">
+                class=" w-full  shadow-inner bg-base-200/60 border-base-content/30 ring-base-content/30 ring-offset-base-content/30 focus:ring-base-content/25 text-base-content/60  rounded-[10px]  z-20 focus:*:text-base-content focus:*:not-italic">
                 <div class="absolute right-3 top-[5.5px] flex gap-2.5 *:size-4 z-20 text-base-content/50">
                     <Icon icon="teenyicons:filter-outline" />
                     <Icon icon="ph:nut" />
