693c0fd7e913c69cb0f43b743dd319fe5813d9d1
lolpockets <auralielol@gmail.com>
2024-08-18 22:01:13 +0000

    Aug 18, 2024, 5:01 PM

--- src/pages/modules/news.vue
+++ src/pages/modules/news.vue
@@ -65,8 +65,10 @@ const NewsArticles: article[] = [
   <div
     class="flex w-full pr-11 ml-5 absolute overflow-x-scroll h-64 content-center gap-5 top-32 scrollbar-hide left-0 !z-1 news-after">
 
-    <div v-for="article in NewsArticles" <embed :src="article.link" type="video/mp4" width="600" height="400">
+    <div v-for="article in NewsArticles"
       class="card !bg-base-100/60 h-56 text-sm shadow-lg first:w-64px first:min-w-96 min-w-56 !z-0 !*:z-0">
+
+      <embed :src="article.link" type="video/mp4" width="600" height="400">
       <figure>
         <img :src="article.image" :alt="article.title + ' image'" />
       </figure>
