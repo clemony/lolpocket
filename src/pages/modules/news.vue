921e1b05dd515f4c7fdfe0954ca6332e5de99df1
lolpockets <auralielol@gmail.com>
2024-08-18 20:24:43 +0000

    Aug 18, 2024, 3:24 PM

--- src/pages/modules/news.vue
+++ src/pages/modules/news.vue
@@ -81,14 +81,12 @@ const NewsArticles: article[] = [
             on {{ article.source2 }}
           </div>
         </div>
-        <div class="absolute bottom-5 right-5">
-          <div
-            class=" absolute btn  btn-sm rounded-full bg-transparent outline-none border-none shadow-none hover:opacity-0 hover:invisible transition-all duration-700">
+        <div class="absolute bottom-5 right-5 *:btn *:btn-sm *:rounded-full *:transition-all *:duration-700">
+          <div class="bg-transparent outline-none border-none shadow-none hover:opacity-0 hover:invisible">
             <Icon v-if="article && article.icon" :icon="article.icon" class="size-5 text-base-content/70" />
           </div>
 
-          <button
-            class="btn rounded-full btn-sm font-normal gap-1 text-xs w-0 opacity-0 hover:opacity-100 hover:w-auto transition-all duration-700 *:transition-all *:duration-700">
+          <button class="font-normal gap-1 text-xs w-0 opacity-0 hover:opacity-100 hover:w-auto">
             <span>Read on {{ article.linkTitle }}</span>
             <Icon icon="cil:external-link" />
           </button>
