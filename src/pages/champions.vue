11ca0ea5b31b50e45d11a05a14478c282d1e4333
lolpockets <auralielol@gmail.com>
2024-08-18 17:39:22 +0000

    Aug 18, 2024, 12:39 PM

--- src/pages/champions.vue
+++ src/pages/champions.vue
@@ -69,7 +69,22 @@ function handleChampionClick(champion: Champion) {
   <div class="drawer drawer-end">
     <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
     <div class="drawer-content">
-      <label for="my-drawer-4" class="drawer-button btn btn-primary">check</label>
+      <!-- Page content here -->
+      <label for="my-drawer-4" class="drawer-button btn btn-primary">Open drawer</label>
+    </div>
+    <div class="drawer-side">
+      <label for="my-drawer-4" aria-label="close sidebar" class="drawer-overlay"></label>
+      <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
+        <!-- Sidebar content here -->
+        <li><a>Sidebar Item 1</a></li>
+        <li><a>Sidebar Item 2</a></li>
+      </ul>
+    </div>
+  </div>
+
+  <div class="drawer drawer-end">
+    <input id="my-drawer-4" type="checkbox" class="drawer-toggle" />
+    <div class="drawer-content">
       <div role="tablist" class="tabs layout !mx-0 !w-full border-b-transparent">
 
         <a role="tab" class="tab tab-active w-44 font-semibold">Champions</a>
