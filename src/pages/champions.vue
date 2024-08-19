2e8d8493bd8b956a12b93a69ce1618ca9f4348e9
lolpockets <auralielol@gmail.com>
2024-08-19 00:09:42 +0000

    Aug 18, 2024, 7:09 PM

--- src/pages/champions.vue
+++ src/pages/champions.vue
@@ -173,7 +173,7 @@ text.replace ('Active)
             class="ability-wrapper join join-vertical w-full bg-base-100 rounded-box mt-5 shadow-lg shadow-neutral/20">
             <div v-for="(ability, key) in selectedChampion.abilities" :key="key"
               class="collapse collapse-arrow join-item">
-              <input type="checkbox" :name="'ability-accordion-' + key" />
+              <input type="radio" name="ability-accordion" />
               <div class="collapse-title">
                 <img :src="ability.img" :alt="ability.name" />
                 <div>{{ ability.name }}</div>
