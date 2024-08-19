dda83f60f90bed2480d4c981531566eb2fe964f4
lolpockets <auralielol@gmail.com>
2024-08-19 04:49:36 +0000

    Aug 18, 2024, 11:49 PM

--- src/pages/modules/news.vue
+++ src/pages/modules/news.vue
@@ -91,7 +91,7 @@ const NewsArticles: article[] = [
           </div>
           <div
             class="group-hover:translate-y-0 group-hover:opacity-100 absolute translate-y-full transition-all duration-700 opacity-0 bg-neutral/80 w-full h-full  top-0 left-0 backdrop-blur-md text-neutral-content rounded-lg overflow-hidden">
-            {{ article.linkTitle }}
+            Read more on {{ article.linkTitle }}
           </div>
 
 
