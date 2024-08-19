1aea24354ace9cc1363eb4e0b51465f4536c3d0c
lolpockets <auralielol@gmail.com>
2024-08-19 00:50:29 +0000

    Aug 18, 2024, 7:50 PM

--- src/pages/champions.vue
+++ src/pages/champions.vue
@@ -51,7 +51,9 @@ const checked = ref('checked');
 console.log(checked);
 
 function abilityClick() {
-  RadioButton.uncheck;
+  if (RadioButton.selected) {
+    RadioButton.selected = false;
+  }
 };
 
 const query = ref('')
@@ -183,7 +185,7 @@ text.replace ('Active)
             class="ability-wrapper join join-vertical w-full bg-base-100 rounded-box mt-5 shadow-lg shadow-neutral/20">
             <div v-for="(ability, key) in selectedChampion.abilities" :key="key"
               class="collapse collapse-arrow join-item" @click="abilityClick">
-              <input ref="abilityCheck" type="radio" name="abilityAccordion" v-model="checked" />
+              <input ref="abilityCheck" type="radio" name="abilityAccordion" v-model="checked" :value="key" />
               <div class="collapse-title">
                 <img :src="ability.img" :alt="ability.name" />
                 <div>{{ ability.name }}</div>
