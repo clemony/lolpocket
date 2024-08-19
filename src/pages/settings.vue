8dcd0b35f544e6addd590da690a8367b19a67e73
lolpockets <auralielol@gmail.com>
2024-08-19 16:03:31 +0000

    Aug 19, 2024, 11:03 AM

--- src/pages/settings.vue
+++ src/pages/settings.vue
@@ -69,21 +69,21 @@ function updateTheme(theme: string) {
         </div>
 
 
-        <div class="grid place-items-stretch w-full grid-cols-3">
+        <div class="grid place-items-stretch w-full grid-cols-3 mt-4">
 
           <template v-for="scheme in schema">
-            <label :for="scheme.name" class="m-1">
+            <label :for="scheme.name" class="m-1 rounded-lg overflow-hidden">
               <div class="w-full h-full" data-act-class="!outline-base-content" :data-set-theme="scheme.key">
                 <div
                   class="w-full h-full rounded-btn !text-xs border border-base-300 shadow-md bg-base-100 text-base-content cursor-pointer"
                   :data-theme="scheme.key">
-                  <div class="grid grid-cols-5 grid-rows-3 w-full h-10 ">
+                  <div class="grid grid-cols-5 grid-rows-3 w-full h-10 rounded-btn">
                     <div class="bg-base-200 col-start-1 row-span-2 row-start-1"></div>
                     <div class="bg-base-300 col-start-1 row-start-3"></div>
                     <div
                       class="bg-base-100 col-span-4 col-start-2 row-span-3 row-start-1 flex  gap-3 place-content-center items-center">
                       <input type="radio" :id="scheme.name" name="ui-scheme"
-                        class="radio radio-base-content bg-base-300" />
+                        class="radio radio-base-200 bg-base-content size-5" />
                       <div class="font-semibold ">{{ scheme.name }}</div>
                     </div>
                   </div>
