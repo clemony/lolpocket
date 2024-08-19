c9e2536e91175a62f879ebea227dd35bec7639ab
lolpockets <auralielol@gmail.com>
2024-08-19 16:28:42 +0000

    Aug 19, 2024, 11:28 AM

--- src/assets/theme.js
+++ src/assets/theme.js
@@ -7,7 +7,7 @@ module.exports = {
     neutral: "oklch( 22.3899% 0.031305 278.07229)",
     "neutral-content": "oklch( 95.8796% 0.008588 247.915135)",
   },
-  light: {
+  dark: {
     "base-100": "oklch( 24.3535% 0 0)",
     "base-200": "oklch( 22.6487% 0 0)",
     "base-300": "oklch( 20.944% 0 0)",
@@ -15,7 +15,7 @@ module.exports = {
     neutral: "oklch( 27.441% 0.01325 253.041249)",
     "neutral-content": "oklch( 85.4882% 0.00265 253.041249)",
   },
-  light: {
+  dim: {
     "base-100": "oklch( 30.8577% 0.023243 264.149498)",
     "base-200": "oklch( 28.0368% 0.01983 264.182074)",
     "base-300": "oklch( 26.3469% 0.018403 262.177739)",
