2d7a0451c30607e635d3d01b012767ee6ff570f1
lolpockets <auralielol@gmail.com>
2024-08-19 12:33:31 +0000

    Aug 19, 2024, 7:33 AM

---
+++ src/assets/theme.js
@@ -0,0 +1,26 @@
+module.exports = {
+  light: {
+    "base-100": "oklch( 100% 0 0)",
+    "base-200": "oklch( 93% 0 0)",
+    "base-300": "oklch( 86% 0 0)",
+    "base-content": "oklch( 22.3899% 0.031305 278.07229)",
+    neutral: "oklch( 22.3899% 0.031305 278.07229)",
+    "neutral-content": "oklch( 95.8796% 0.008588 247.915135)",
+  },
+  light: {
+    "base-100": "oklch( 24.3535% 0 0)",
+    "base-200": "oklch( 22.6487% 0 0)",
+    "base-300": "oklch( 20.944% 0 0)",
+    "base-content": "oklch( 84.8707% 0 0)",
+    neutral: "oklch( 27.441% 0.01325 253.041249)",
+    "neutral-content": "oklch( 85.4882% 0.00265 253.041249)",
+  },
+  light: {
+    "base-100": "oklch( 30.8577% 0.023243 264.149498)",
+    "base-200": "oklch( 28.0368% 0.01983 264.182074)",
+    "base-300": "oklch( 26.3469% 0.018403 262.177739)",
+    "base-content": "oklch( 82.9011% 0.031335 222.959324)",
+    neutral: "oklch( 24.7311% 0.020483 264.094728)",
+    "neutral-content": "oklch( 82.9011% 0.031335 222.959324)",
+  },
+};
