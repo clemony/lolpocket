94f12f6e219adee3eed7c629cb4f39d08659a3b0
lolpockets <auralielol@gmail.com>
2024-08-19 21:42:50 +0000

    Aug 19, 2024, 4:42 PM

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -170,7 +170,7 @@ onMounted(async () => {
       <News v-if="isHome" class="!overflow-x-scroll " />
       <div class="h-[36px] w-full bg-base-200"></div>
       <div ref="nav"
-        class="overscroll-none bg-base-100/50 shadow-lg backdrop-blur-md nav h-full w-full  bg-blend-lighten backdrop-brightness-105">
+        class="overscroll-none bg-base-100/50 shadow-lg backdrop-blur-md nav h-full w-full  bg-blend-screen backdrop-brightness-125">
 
         <Tree v-model:selectionKeys="selectedKey" :value="nodes" selectionMode="single" :metaKeySelection="false"
           @nodeSelect="onNodeSelect" id="tree">
