0c9e6913e0c789d28f11ecf060abb0ad07d6509a
lolpockets <auralielol@gmail.com>
2024-08-19 12:35:44 +0000

    Aug 19, 2024, 7:35 AM

--- src/lib/index.d.ts
+++ src/lib/index.d.ts
@@ -1,3 +1,8 @@
+import type { CustomTheme, Theme } from "@/assets/theme.js"
+
+declare const themes: Record<`${Theme}`, CustomTheme[string]>
+
+export = themes
 declare module 'vue3-resize-text';
 declare module '@iconify/vue';
 declare module '@/pages/tree.vue';
