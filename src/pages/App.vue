c77c660807ab36b6c1881a10bc82c8936118aeef
lolpockets <auralielol@gmail.com>
2024-08-20 11:05:59 +0000

    Aug 20, 2024, 6:05 AM

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -162,14 +162,16 @@ onMounted(async () => {
     <SplitterPanel ref="menuRef" id="menu" class=" m-0 p-0 transition-all duration-700 delay-200 z-0">
 
       <News v-if="isHome" class="!overflow-x-scroll " />
-      <div class="z-10 shadow-lg">
-
-
-
-
+      <div ref="nav"
+        class="overscroll-none bg-base-100/50 shadow-lg backdrop-blur-md nav h-full w-full  bg-blend-screen backdrop-brightness-125 z-10 ">
         <div class="h-[36px] w-full bg-base-200"></div>
-        <div ref="nav"
-          class="overscroll-none bg-base-100/50 shadow-lg backdrop-blur-md nav h-full w-full  bg-blend-screen backdrop-brightness-125">
+
+
+
+
+
+
+        <div>
 
           <Tree v-model:selectionKeys="selectedKey" :value="nodes" selectionMode="single" :metaKeySelection="false"
             @nodeSelect="onNodeSelect" id="tree">
