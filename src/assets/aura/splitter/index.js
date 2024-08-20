427285523452b8e54676c5445c0a8a71205dceab
lolpockets <auralielol@gmail.com>
2024-08-20 12:36:52 +0000

    Aug 20, 2024, 7:36 AM

--- src/assets/aura/splitter/index.js
+++ src/assets/aura/splitter/index.js
@@ -24,7 +24,7 @@ export default {
       "flex",
       "items-center",
       "justify-center",
-      "shrink-0",
+      "shrink-0 pb-0 mb-0",
 
       // Colors
       "bg-base-300/70 bg-blend-screen backdrop-brightness-110 backdrop-blur-md",
