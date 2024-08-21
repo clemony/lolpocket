6da2348f080045e96d1accb9d8f89a144fde2c6e
lolpockets <auralielol@gmail.com>
2024-08-21 22:23:47 +0000

    Aug 21, 2024, 5:23 PM

---
+++ src/stores/news.ts
@@ -0,0 +1,62 @@
+export interface article {
+title: string
+source: string
+icon?: string
+source2?: string
+image: string
+link?: string
+linkTitle?: string
+}
+
+
+export const NewsArticles: article[] = [
+{
+title: 'Patch 14.15',
+source: 'Riot Games',
+icon: 'simple-icons:riotgames',
+image:
+'https://cmsassets.rgpub.io/sanity/images/dsfx7636/news/24eb05be171a372ed26b0b6034bf875b3bd09d98-2560x1440.png?auto=format&fit=fill&q=80&w=960',
+},
+{
+title: 'PBE Changes',
+source2: ' /u/FrankTheBoxMonster',
+source: 'Reddit',
+icon: 'fa:reddit',
+image:
+'/img/art/magic.webp',
+link: 'https://www.reddit.com/r/leagueoflegends/wiki/pbechanges/',
+linkTitle: 'Reddit.com',
+},
+{
+title: 'Champ Flavor Tier List',
+source: 'Lolpockets Team',
+source2: 'Clem',
+icon: 'fluent-emoji-high-contrast:kissing-cat',
+image:
+'/img/art/chase.webp',
+},
+{
+title: 'Champ Flavor Tier List',
+source: 'Lolpockets Team',
+source2: 'Clem',
+icon: 'fluent-emoji-high-contrast:kissing-cat',
+image:
+'/img/art/chase.webp',
+},
+{
+title: 'Champ Flavor Tier List',
+source: 'Lolpockets Team',
+source2: 'Clem',
+icon: 'fluent-emoji-high-contrast:kissing-cat',
+image:
+'/img/art/chase.webp',
+},
+{
+title: 'Champ Flavor Tier List',
+source: 'Lolpockets Team',
+source2: 'Clem',
+icon: 'fluent-emoji-high-contrast:kissing-cat',
+image:
+'/img/art/chase.webp',
+},
+]
