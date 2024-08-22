413fef7c3b991fef7839275fe600d500589b8106
lolpockets <auralielol@gmail.com>
2024-08-22 02:16:24 +0000

    Aug 21, 2024, 9:16 PM

--- src/pages/App.vue
+++ src/pages/App.vue
@@ -96,8 +96,7 @@ function test(event: any) {
     console.log(showDropdowns);
     isMinimized.value = true;   // Add 'minimize' class
     firstPanelSize.value = 3;   // Set size to 5
-    showDropdowns.value = [false, false, true];
-    console.log(showDropdowns);
+    showDropdowns[1].value = false;
   } else {
     isMinimized.value = false;  // Remove 'minimize' class
     firstPanelSize.value = firstSize; // Maintain the dynamic size from the splitter
