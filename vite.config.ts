02ec7851897f73b365b0c062349a69cd74acc0dd
lolpockets <auralielol@gmail.com>
2024-09-05 11:26:54 -0500

    hi

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,6 @@
 import vue from '@vitejs/plugin-vue';
 import AutoImport from 'unplugin-auto-import/vite';
 import IconsResolver from 'unplugin-icons/resolver';
-<<<<<<< HEAD
-import Components from "unplugin-vue-components/vite";
-import VueRouter from 'unplugin-vue-router/vite'
-import { VueRouterAutoImports } from 'unplugin-vue-router'
-import { defineConfig } from "vite";
-import { ViteAliases } from "vite-aliases";
-
-=======
 import Icons from 'unplugin-icons/vite';
 import Components from 'unplugin-vue-components/vite';
 import VueRouter from 'unplugin-vue-router/vite';
@@ -17,26 +9,10 @@ import { defineConfig } from 'vite';
 import { ViteAliases } from 'vite-aliases';
 import { promises as fs } from 'node:fs';
 import { FileSystemIconLoader } from 'unplugin-icons/loaders';
->>>>>>> 0d8a5d3 (fix?)
 
 export default defineConfig({
   plugins: [
     VueRouter({
-<<<<<<< HEAD
-       // what files should be considered as a pages
-  extensions: ['.vue'],
-  dts: './typed-router.d.ts',
-
- } ),
-    vue(),
-    Components({
-      dirs: ["src/pages/modules", "src/pages"], // Ensure paths are correct
-      extensions: ["vue"],
-      deep: true,
-      dts: "./components.d.ts",
-      resolvers: [
-        IconsResolver(),
-=======
       // what files should be considered as a pages
       extensions: ['.vue'],
       dts: './typed-router.d.ts',
@@ -52,7 +28,6 @@ export default defineConfig({
           enabledCollections: ['ui-icons'],
           customCollections: ['ui-icons'],
         }),
->>>>>>> 0d8a5d3 (fix?)
       ],
       include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
     }),
@@ -64,22 +39,11 @@ export default defineConfig({
         /\.vue\?vue/, // .vue
         /\.md$/, // .md
       ],
-<<<<<<< HEAD
-      imports: [
-        'vue',
-        VueRouterAutoImports, 
-
-      ],
-      dirs: [
-        '@stores/', 
-        '@lib/', 
-=======
       imports: ['vue', VueRouterAutoImports],
       dirs: [
         '@iconify/vue',
         './src/utils',
         './src/stores',
->>>>>>> 0d8a5d3 (fix?)
         // ...
       ],
       eslintrc: {
@@ -89,13 +53,8 @@ export default defineConfig({
 
       vueTemplate: true,
       viteOptimizeDeps: true,
-<<<<<<< HEAD
-      
-      dts: "./auto-imports.d.ts", // Generates `auto-imports.d.ts` file
-=======
 
       dts: './auto-imports.d.ts', // Generates `auto-imports.d.ts` file
->>>>>>> 0d8a5d3 (fix?)
     }),
     ViteAliases({
       prefix: '@',
@@ -105,15 +64,11 @@ export default defineConfig({
       logPath: 'src/logs',
       createGlobalAlias: true,
     }),
-<<<<<<< HEAD
-    
-=======
     Icons({
       customCollections: {
         'ui-icons': FileSystemIconLoader('./src/assets/img', (svg) => svg.replace(/^<svg /, '<svg fill="currentColor" stroke="currentColor" ')),
       },
     }),
->>>>>>> 0d8a5d3 (fix?)
   ],
   clearScreen: false,
 
