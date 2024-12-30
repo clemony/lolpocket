f80259173f754306d08b05cb1a70b870a5d21415
lolpockets <auralielol@gmail.com>
2024-12-30 17:54:45 -0600

    update .eslintrc-auto-import.json, auto-imports.d.ts, components.d.ts and Google.vue

--- src/components/app/login/Google.vue
+++ src/components/app/login/Google.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { supabase } from '@lib/supabase'
-
 const googleLogin = () =>
     supabase.auth.signInWithOAuth({
         provider: 'google',
@@ -16,21 +15,5 @@ const googleLogin = () =>
         <icon icon="mdi:google" class="-ml-6.5 size-6" />
         <div class="">Log in with Google</div>
     </Button>
-
-    <!--
-        <div
-            id="g_id_onload"
-            data-client_id="YOUR_GOOGLE_CLIENT_ID"
-            data-login_uri="https://your.domain/your_login_endpoint"
-            data-auto_prompt="false"></div>
-        <div
-            class="g_id_signin"
-            data-type="standard"
-            data-size="large"
-            data-theme="outline"
-            data-text="sign_in_with"
-            data-shape="rectangular"
-            data-logo_alignment="left"></div>
-    -->
 </template>
 <style scoped></style>
