cff7533896ea602a263d40b58b4361f0255aa041
lolpockets <auralielol@gmail.com>
2025-01-06 17:46:19 -0600

    update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string
-const supabaseKey = import.meta.env.VITE_SUPABASE_KEY as string
+const supabaseUrl = 'https://zsxuzlzfwovsbzzrpcbc.supabase.co'
+const supabaseKey =
+    '=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpzeHV6bHpmd292c2J6enJwY2JjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzUyNDg3MDcsImV4cCI6MjA1MDgyNDcwN30.JhLNIN3WK6_0T_SRVunioNNGyiJ1BpwSjGXS7WaVszU'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
