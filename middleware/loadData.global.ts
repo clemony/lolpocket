ad1f5cc210fe33fa2ad40f02496a44f4be73fb5c
lolpockets <auralielol@gmail.com>
2025-01-16 17:13:51 -0600

    update loadData.global.ts

--- middleware/loadData.global.ts
+++ middleware/loadData.global.ts
@@ -7,7 +7,7 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   }
 
   !ds.champions.length ? getChampsFromDatabase() : ''
-  !ds.SRitems.length ? getItemsFromDatabase() : ''
+  !ds.items.length ? getItemsFromDatabase() : ''
   !ds.paths.length ? getRuneData() : ''
   //console.log('💠 - useDataStore - SRitems:', ds.SRitems);
   //console.log('💠 - champions:', ds.champions);
