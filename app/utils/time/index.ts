7799daa9633c0b97ab4a054ecf9fa341c3d08435
clem <15641617+clemony@users.noreply.github.com>
2025-11-05 00:21:35 -0600

    update app/components/tiptap/index.ts, app/composables/index.ts, app/utils/pocket/index.ts and app/utils/time/index.ts

--- app/utils/time/index.ts
+++ app/utils/time/index.ts
@@ -1,7 +1,10 @@
 export * from './formatTimeAgo';
+export * from './generateSummonerLinks';
 export * from './getGreeting';
 export * from './getISOWeek';
+export * from './getParentRoute';
 export * from './isStale';
 export * from './parseDate';
 export * from './parseISOString';
 export * from './patchDate';
+
